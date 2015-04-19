53c7d95ff27f528083bb49373a615687ef59daf8
Peter Csikos <stuff@wavetrip.org>
2015-04-19 15:39:22 +0200

    Add new sc apikey

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,10 +3,10 @@
 angular.module('sc2App').constant({
     soundcloudConfig: {
         apiBaseUrl: 'http://api.soundcloud.com',
-        // apiKey: '03ef56ca3245389357addc704af72a54',
-        apiKey: '14c6b266c11792a6fcf3a05c809c1196',
-        // redirectUri: 'http://sc.local:9000/callback.html',
-        redirectUri:'http://wavetrip.org/sc/callback.html',
+        apiKey: 'fdb4bfea50165446d63c898644f6c475',
+        // apiKey: 'b9d11449cd4c64d461b8b5c30650cd06',
+        redirectUri: 'http://sc2.local:9000/callback.html',
+        // redirectUri:'http://wavetrip.org/sc2/callback.html',
         waveformServiceUrl: 'http://wis.sndcdn.com/'
     },
 });
