152e0aabbfb973013adbf1751d96924841388bf5
Peter Csikos <stuff@wavetrip.org>
2015-05-02 17:30:42 +0200

    Add fullscreen osc

--- app/scripts/controllers/stream.js
+++ app/scripts/controllers/stream.js
@@ -11,6 +11,7 @@ angular.module('sc2App').controller('streamCtrl', function ($scope, $window, $ht
     $scope.storedToken = localStorageService.get('accessToken');
     $scope.user = localStorageService.get('user');
     $scope.showReposts = true;
+    $scope.fsScope = false;
     $scope.stream = [];
     $scope.likedTracks = [];
     $scope.playerData = {
@@ -316,6 +317,10 @@ angular.module('sc2App').controller('streamCtrl', function ($scope, $window, $ht
         },
         toggleReposts: function() {
             $scope.showReposts = !$scope.showReposts;
+        },
+        toggleOsc: function() {
+            animation.x3dscope = !animation.x3dscope;
+            $scope.fsScope = animation.x3dscope;
         }
     };
 
--- app/scripts/services/animation.js
+++ app/scripts/services/animation.js
@@ -1,27 +1,38 @@
 'use strict';
 
-angular.module('sc2App').factory('animation', function ($window, audioContext, canvasService) {
+angular.module('sc2App').factory('animation', function ($window, audioContext, canvasService, AdditiveBlendShader) {
     var self = this,
         spacerWidth = 30,
         barWidth = 28,
         numBars = 15,
         magnitude;
 
+    var renderComposer, glowComposer, blendComposer;
+    var THREE = $window.THREE;
+
+    var camera, scene, webglRenderer;
+    var renderPass, filmPass, hblurPass, vblurPass;
+    var oscLine;
+
     self.requestId = null;
     var analyserData = new Uint8Array(audioContext.analyser.frequencyBinCount);
     var oscData = new Uint8Array(audioContext.osc.frequencyBinCount);
 
     canvasService.oscContext.lineWidth = 3;
-    // canvasService.oscContext.strokeStyle = 'rgba(255,255,255,0.7)';
     canvasService.oscContext.strokeStyle = '#ffffff';
-    // canvasService.analyserTopContext.strokeStyle = 'rgba(255,255,255,0.7)';
     canvasService.analyserTopContext.strokeStyle = '#ffffff';
     canvasService.analyserTopContext.lineCap = 'round';
     canvasService.analyserTopContext.lineWidth = 4;
 
+    self.x3dscope = false;
+
     self.animate = function() {
         self.requestId = $window.requestAnimationFrame(self.animate);
-        loop();
+        if (self.x3dscope === true) {
+            x3Dloop();
+        } else {
+            loop();
+        }
     };
 
     self.killAnimation = function() {
@@ -31,6 +42,102 @@ angular.module('sc2App').factory('animation', function ($window, audioContext, c
             // canvasService.analyserTopContext.clearRect(0, 0, 450, 100);
             // canvasService.oscContext.clearRect(0, 0, canvasService.oscContext.canvas.width, 300);
         }
+    };
+
+    var init3D = function() {
+
+        var screenWidth = 800,
+            screenHeight = 600;
+
+        var container = document.getElementById('webGLWrapper');
+
+        camera = new THREE.PerspectiveCamera(1, screenWidth / screenHeight, 1, 1000000);
+        camera.position.z = 30000;
+
+        scene = new THREE.Scene();
+
+        var material = new THREE.LineBasicMaterial({
+            color: 0x00ffff
+        });
+
+        var oscGeometry = new THREE.Geometry();
+
+        for (var o = 0; o < 1024; o++) {
+            oscGeometry.vertices.push(new THREE.Vector3( o*1, 0, 0 ));
+        }
+
+        oscLine = new THREE.Line(oscGeometry, material);
+        scene.add(oscLine);
+        oscLine.position.x = -512;
+        oscLine.position.y = -100;
+        scene.add(new THREE.AmbientLight(0xffffff));
+
+        // renderer
+        webglRenderer = new THREE.WebGLRenderer();
+        webglRenderer.setSize(1920, 955);
+        container.appendChild(webglRenderer.domElement);
+
+        // postprocessing
+        var blurriness = 3;
+        var renderTargetParameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBufer: false };
+
+        // base layer
+        var renderTarget = new THREE.WebGLRenderTarget( screenWidth, screenHeight, renderTargetParameters );
+        renderComposer = new THREE.EffectComposer( webglRenderer, renderTarget);
+        renderPass = new THREE.RenderPass( scene, camera );
+        var copyPass = new THREE.ShaderPass( THREE.CopyShader );
+        var bloomPass = new THREE.BloomPass(3, 8, 2.0, 512);
+
+        renderComposer.addPass( renderPass );
+        renderComposer.addPass( copyPass );
+        renderComposer.addPass( bloomPass );
+
+        // glow layer
+        hblurPass = new THREE.ShaderPass( THREE.HorizontalBlurShader );
+        vblurPass = new THREE.ShaderPass( THREE.VerticalBlurShader );
+        hblurPass.uniforms.h.value = blurriness/screenWidth;
+        vblurPass.uniforms.v.value = blurriness/screenHeight;
+        copyPass = new THREE.ShaderPass( THREE.CopyShader );
+        var renderTargetGlow = new THREE.WebGLRenderTarget( screenWidth/4, screenHeight/4, renderTargetParameters );
+        glowComposer = new THREE.EffectComposer( webglRenderer,renderTargetGlow);
+        glowComposer.addPass( copyPass );
+        glowComposer.addPass( renderPass );
+        glowComposer.addPass( bloomPass );
+        glowComposer.addPass( hblurPass );
+        glowComposer.addPass( vblurPass );
+        glowComposer.addPass( hblurPass );
+        glowComposer.addPass( vblurPass );
+
+        // blending
+        blendComposer = new THREE.EffectComposer( webglRenderer );
+        var blendPass = new THREE.ShaderPass( AdditiveBlendShader );
+        blendPass.uniforms.tBase.value = renderComposer.renderTarget1;
+        blendPass.uniforms.tAdd.value = glowComposer.renderTarget1;
+        blendPass.uniforms.amount.value = 0;
+        blendComposer.addPass( blendPass );
+
+        filmPass = new THREE.ShaderPass( THREE.FilmShader );
+        filmPass.uniforms.grayscale.value = 0;
+        filmPass.uniforms.sIntensity.value = 0.8;
+        filmPass.uniforms.sCount.value = screenHeight;
+
+        blendComposer.addPass( filmPass );
+        filmPass.renderToScreen = true;
+    };
+
+    init3D();
+
+    var x3Dloop = function() {
+        audioContext.osc.getByteTimeDomainData(oscData);
+
+        for (var k = 0; k < 1024; k++) {
+            oscLine.geometry.vertices[k].y = oscData[k];
+        }
+        oscLine.geometry.verticesNeedUpdate = true;
+
+        renderComposer.render( 0.1 );
+        glowComposer.render( 0.1 );
+        blendComposer.render( 0.1 );
     };
 
     var loop = function() {
